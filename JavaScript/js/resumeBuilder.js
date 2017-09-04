a39d56f0c1dcd66b8e49b93f63d29d883b044518
Jhean <jheanmarllos1@gmail.com>
2017-09-04 11:30:16 -0300

    adding the directory js to help in another projects

---
+++ JavaScript/js/resumeBuilder.js
@@ -0,0 +1,9 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+ $("#main"). append("Jhean Marllos");
+ var awesomeThoughts = "I am Jhean Marllos and I am Awesome";
+ var funThoughts = awesomeThoughts.replace("Awesome", "Fun");
+ console.log(awesomeThoughts);
+ console.log(funThoughts);
+  $("#main"). append(" "+funThoughts);
